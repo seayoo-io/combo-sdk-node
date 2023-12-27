8366a407c9215ed03a87b23e6822ce30b9772337
马超 <machao7@kingsoft.com>
2023-12-27 21:03:46 +0800

    fix: 修复读取版本号的兼容处理问题

--- src/api/userAgent.ts
+++ src/api/userAgent.ts
@@ -1,9 +1,12 @@
-import { readFileSync } from "node:fs"
+import { existsSync, readFileSync } from "node:fs"
 import { platform, arch } from "node:os"
 import { resolve } from "node:path"
 import { SDKName } from "../const"
-const packageJson = readFileSync(resolve(__dirname, "../../package.json"), "utf8")
-const { version } = JSON.parse(packageJson)
+
+const pkg = resolve(__dirname, "../package.json")
+const pkgString = existsSync(pkg) ? readFileSync(pkg, "utf8") : JSON.stringify({ version: "0.0.0" })
+const pkgJson = JSON.parse(pkgString)
+const version = pkgJson && typeof pkgJson === "object" && "version" in pkgJson ? `${pkgJson.version}` : "0.0.0"
 const nodeVersion = process.version
 
 export function getUserAgent(game: string) {
