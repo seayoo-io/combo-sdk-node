fbc7dbe5ac1bc68b4ec89cf0f3bf9d618fb62597
马超 <machao7@kingsoft.com>
2023-12-28 11:03:31 +0800

    perf: 优化暴露的签名工具方法

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ export { NotificationType } from "./notify/types"
 export type { NotificationHandler } from "./notify/types"
 
 export type { SDKBaseConfig } from "./utils"
-export { calcAuthorizationHeader, checkHttpAuthInfo } from "./utils"
+export { parseAuthorizationHeader, calcAuthorizationHeader } from "./utils"
 export { Endpoint, Platform, IdP } from "./const"
--- src/utils/signer.ts
+++ src/utils/signer.ts
@@ -4,6 +4,7 @@ import type { IRequestOptions } from "../request"
 export const AuthorizationField = "Authorization"
 const MaxTimeDiff = 5 * 60 * 1000
 const EmptyStringSha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+const timestampReg = /^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z$/
 
 interface SignDataOption {
   game: string
@@ -14,6 +15,7 @@ interface SignDataOption {
   data: IRequestOptions["body"]
   timestamp?: string
 }
+type SignDataOptionForVerify = Omit<SignDataOption, "game" | "timestamp"> & { game?: string }
 
 // 签名方法定义，给未来扩展新签名方法留出空间
 const SigningDefinitions = {
@@ -27,15 +29,15 @@ const SigningDefinitions = {
       const signature = hS256(secret, [this.prefix, method.toUpperCase(), uri, ts, payloadHash].join("\n"))
       return `${this.prefix} Game=${game},Timestamp=${ts},Signature=${signature}`
     },
-    verify(authHeader, option): boolean {
-      const info = parseAuthHeader(authHeader, this.prefix)
+    verify(authString, option): boolean {
+      const info = parseRawAuthHeader(authString, this.prefix)
       if (!info) {
         return false
       }
       const game = "Game" in info ? info.Game : null
       const timestamp = "Timestamp" in info ? info.Timestamp : null
       const signature = "Signature" in info ? info.Signature : null
-      if (!game || !signature || !timestamp || option.game !== game) {
+      if (!game || !signature || !timestamp || (option.game && option.game !== game)) {
         return false
       }
       const ts = parseTimestamp(info.timestamp)
@@ -43,7 +45,7 @@ const SigningDefinitions = {
       if (!ts || ts < now - MaxTimeDiff || ts > now + MaxTimeDiff) {
         return false
       }
-      return info.signature === this.sign({ ...option, timestamp })
+      return info.signature === this.sign({ ...option, game, timestamp })
     },
   },
 } as const satisfies Record<
@@ -51,17 +53,27 @@ const SigningDefinitions = {
   {
     prefix: string
     sign: (option: SignDataOption) => string
-    verify: (authHeader: string, option: SignDataOption) => boolean
+    verify: (authString: string, option: SignDataOptionForVerify) => boolean
   }
 >
 
-const timestampReg = /^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z$/
+/**
+ * 格式化 AuthorizationHeader
+ */
+export function parseAuthorizationHeader(authString: string, version: keyof typeof SigningDefinitions = "HS256") {
+  if (!version || !SigningDefinitions[version]) {
+    console.error(`parseAuthorizationHeader: invalid version ${version}, support ${Object.keys(SigningDefinitions).join(", ")}`)
+    return null
+  }
+  return parseRawAuthHeader(authString, SigningDefinitions[version].prefix)
+}
 
 /**
  * 获取请求签名值，默认签名方法是 HS256
  */
 export function calcAuthorizationHeader(option: SignDataOption, version: keyof typeof SigningDefinitions = "HS256") {
   if (!version || !SigningDefinitions[version]) {
+    console.error(`calcAuthorizationHeader: invalid version ${version}, support ${Object.keys(SigningDefinitions).join(", ")}`)
     return "ErrorSigningVersion"
   }
   return SigningDefinitions[version].sign(option)
@@ -70,14 +82,14 @@ export function calcAuthorizationHeader(option: SignDataOption, version: keyof t
 /**
  * 检查 http auth 信息是否正确
  */
-export function checkHttpAuthInfo(authHeader: string, option: SignDataOption): boolean {
-  if (!authHeader) {
+export function checkHttpAuthInfo(authString: string, option: SignDataOptionForVerify): boolean {
+  if (!authString) {
     return false
   }
   const cfgs = Object.values(SigningDefinitions)
   for (const cfg of cfgs) {
-    if (authHeader.startsWith(cfg.prefix + " ")) {
-      return cfg.verify(authHeader, option)
+    if (authString.startsWith(cfg.prefix + " ")) {
+      return cfg.verify(authString, option)
     }
   }
   return false
@@ -107,12 +119,12 @@ function hS256(secret: string, data: string) {
   return createHmac("sha256", secret).update(data).digest("hex").toLowerCase()
 }
 
-function parseAuthHeader(authHeader: string, prefix: string): Record<string, string> | null {
-  if (!authHeader || !authHeader.startsWith(prefix + " ")) {
+function parseRawAuthHeader(authString: string, prefix: string): Record<string, string> | null {
+  if (!authString || !authString.startsWith(prefix + " ")) {
     return null
   }
   return Object.fromEntries(
-    authHeader
+    authString
       .substring(prefix.length + 1)
       .split(",")
       .map<[string, string] | null>((kvs) => {
